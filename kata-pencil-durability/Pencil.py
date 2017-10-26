25c6f42811f7fb10b93bc693089dd03dbe165640
Kojo Appiah <kojo.e.appiah@gmail.com>
2017-10-26 14:48:27 -0400

    Finished Durability Test

---
+++ kata-pencil-durability/Pencil.py
@@ -0,0 +1,11 @@
+class Pencil:
+
+    def __init__(self):
+        self.paper =""
+
+    def write(self, text):
+        self.paper += text
+
+    def print(self):
+        print(paper)
+        return paper
--- kata-pencil-durability/kata_pencil_durability.py
+++ kata-pencil-durability/kata_pencil_durability.py
@@ -1,1 +1,41 @@
+import unittest
 
+class Pencil:
+
+    def __init__(self, durability):
+        self.paper =""
+        self.durability = durability
+
+    def write(self, text):
+        if self.durability - len(text) > 0:
+            self.paper += text
+        else:
+            x = 0
+            while x < self.durability:
+                self.paper += text[x]
+                x += 1
+
+            while x < len(text):
+                self.paper += " "
+                x += 1
+
+    def print(self):
+        print(self.paper)
+        return self.paper
+
+class PencilTests(unittest.TestCase):
+
+    def test_write(self):
+        pencil = Pencil(20)
+        pencil.write("Hello there")
+        self.assertEqual("Hello there", pencil.print())
+
+    def test_durabililty_decline(self):
+        pencil = Pencil(5)
+        pencil.write("1111111")
+        self.assertEqual("11111  ", pencil.print())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
