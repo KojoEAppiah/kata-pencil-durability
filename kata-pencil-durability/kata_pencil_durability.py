6aede49faa08d1e5f6d56d7c6a164eb3b774f3de
Kojo Appiah <kojo.e.appiah@gmail.com>
2017-10-26 16:31:50 -0400

    Finished Upper and Lower Case Durability Decline Tests

--- kata-pencil-durability/kata_pencil_durability.py
+++ kata-pencil-durability/kata_pencil_durability.py
@@ -4,24 +4,47 @@ class Pencil:
 
     def __init__(self, durability):
         self.paper =""
+        self.initial_durability = durability
         self.durability = durability
 
     def write(self, text):
         if self.durability - len(text) > 0:
             self.paper += text
+            self.durability -= len(text)
         else:
+            start_durability = self.durability
             x = 0
-            while x < self.durability:
-                self.paper += text[x]
+            char_count = 0
+            while self.durability > 0:
+                if text[x].isupper():
+                   self.durability -= 2
+                else:
+                    self.durability -= 1
+
+                if self.durability >= 0:
+                    self.paper += text[x]
+                    char_count += 1
+
                 x += 1
 
+            x = char_count
             while x < len(text):
                 self.paper += " "
                 x += 1
 
+            self.durability = 0
+
+
+    def sharpen(self):
+        self.durability = self.initial_durability
+
+    def getDurability(self):
+        return self.durability
+
     def print(self):
         print(self.paper)
         return self.paper
+
 
 class PencilTests(unittest.TestCase):
 
@@ -30,12 +53,22 @@ class PencilTests(unittest.TestCase):
         pencil.write("Hello there")
         self.assertEqual("Hello there", pencil.print())
 
-    def test_durabililty_decline(self):
+    def test_durabililty_decline_lowercase(self):
         pencil = Pencil(5)
-        pencil.write("1111111")
-        self.assertEqual("11111  ", pencil.print())
+        pencil.write("abcdefghi")
+        self.assertEqual("abcde    ", pencil.print())
 
+    def test_durabililty_decline_uppercase(self):
+        pencil = Pencil(5)
+        pencil.write("ABCDEFG")
+        self.assertEqual("AB     ", pencil.print())
 
+    def test_sharpen(self):
+        pencil = Pencil(5)
+        pencil.write("123")
+        self.assertEqual(2, pencil.durability)
+        pencil.sharpen()
+        self.assertEqual(5, pencil.durability)
 
 if __name__ == '__main__':
     unittest.main()
